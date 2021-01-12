4626f0bb70c38bb87bc58718268f86b166e401fc
Aviral-Omar <aviralomar0301@gmail.com>
2021-01-13 03:23:20 +0530

    connected to db

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+
+const MONGODB_URI =
+  'mongodb+srv://aviral:dNT8VD7PBLQ5tF30@cluster0.yrqwv.mongodb.net/cable-prices?retryWrites=true&w=majority';
+
 const app = express();
 
-app.listen(8080);
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
