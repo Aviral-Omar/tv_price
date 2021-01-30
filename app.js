485f3017cc13e6fa762e9cb2206e0fe113e53fc7
Aviral-Omar <aviralomar0301@gmail.com>
2021-01-31 00:26:31 +0530

    fixed minor bug

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose
   })
   .then(() => {
     let port = process.env.PORT;
-    if (port == null || port == '') {
+    if (port == null || port === '') {
       port = 3000;
     }
     app.listen(port);
