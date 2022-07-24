b667499f395ec5bef95a9cca5931d696ba6d387f
Aviral Omar <aviralomar0301@gmail.com>
2022-07-24 19:12:25 +0530

    allowed cors

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const providersRoutes = require('./routes/providers');
 const channelsRoutes = require('./routes/channels');
 const bouquetsRoutes = require('./routes/bouquets');
 
 const MONGODB_URI =
-  'mongodb+srv://aviral:dNT8VD7PBLQ5tF30@cluster0.yrqwv.mongodb.net/cable-prices?retryWrites=true&w=majority';
+	'mongodb+srv://aviral:dNT8VD7PBLQ5tF30@cluster0.yrqwv.mongodb.net/cable-prices?retryWrites=true&w=majority';
 
 const app = express();
 
+app.use(
+	cors({
+		origin: 'https://channel-selector-app.vercel.app',
+	}),
+);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
 });
 
 app.use(providersRoutes);
@@ -25,16 +31,16 @@ app.use(channelsRoutes);
 app.use(bouquetsRoutes);
 
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    let port = process.env.PORT;
-    if (port == null || port === '') {
-      port = 3000;
-    }
-    app.listen(port);
-  })
-  .catch(err => console.log(err));
+	.connect(MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		let port = process.env.PORT;
+		if (port == null || port === '') {
+			port = 3000;
+		}
+		app.listen(port);
+	})
+	.catch(err => console.log(err));
